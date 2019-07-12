01f962b9e9f02c30333c185aa6ecb4e205d4ee93
oussamasouissi <souissi.oussama11@gmail.com>
2019-07-12 15:30:42 +0100

    Integration template admin

--- e-commerce/app/controllers/welcome_controller.rb
+++ e-commerce/app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def home
   end
+  def homeAdmin
+  end
 end
--- e-commerce/config/routes.rb
+++ e-commerce/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/homeAdmin'
   devise_for :users
   get 'welcome/home'
   root 'home#index'

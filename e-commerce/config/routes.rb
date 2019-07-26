fed5004367e96811b6311212802475292f45df24
bondka <https://github.com/Siwarr>
2019-07-26 12:56:50 +0100

    Page about us

--- e-commerce/config/routes.rb
+++ e-commerce/config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
 
   get 'user/home'
   root 'home#index'
+  get "/:page" => "static#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

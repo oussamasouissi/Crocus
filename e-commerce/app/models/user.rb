85a3c1fd53202032176f2cd38d15b8780b3bf382
khawlaabdou <khawla.abd@tek-up.tn>
2019-07-26 16:25:58 +0100

    decomposer accessite

--- e-commerce/app/models/user.rb
+++ e-commerce/app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
+<<<<<<< HEAD
   :recoverable, :rememberable, :validatable ,:omniauthable, :omniauth_providers => [:facebook]
 
   def self.new_with_session(params, session)
@@ -29,5 +30,5 @@ class User < ApplicationRecord
       user.Name = auth.info.name   # assuming the user model has a name
       user.UserName = auth.info.name
     end
-  end
+  end  
 end

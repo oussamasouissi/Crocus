03f5ff4b9a1fb8b3dec4df5caddc2f238ac58023
bondka <https://github.com/Siwarr>
2019-08-01 14:43:55 +0100

    comments with CRUD

---
+++ e-commerce/app/assets/javascripts/applicationRAty.js
@@ -0,0 +1,22 @@
+  $('#star-log').raty({
+    target     : '#hint-log',
+    targetType : 'score',
+    targetKeep : true
+});
+$('#star-comm').raty({
+    target     : '#hint-comm',
+    targetType : 'score',
+    targetKeep : true
+});
+$('#star-tech').raty({
+    target     : '#hint-tech',
+    targetType : 'score',
+    targetKeep : true
+
+});
+$('#star-overall').raty({
+    target     : '#hint-overall',
+    targetType : 'score',
+    targetKeep : true,
+    readOnly   : true
+});
--- e-commerce/app/controllers/comments_controller.rb
+++ e-commerce/app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
-	 def create
+  def create
     @produit = Produit.find(params[:produit_id])
     @comment = @produit.comments.create(comment_params)
     redirect_to produit_path(@produit)
   end
+
+
+  def destroy
+    @produit = Produit.find(params[:produit_id])
+    @comment = @produit.comments.find(params[:id])
+    @comment.destroy
+    redirect_to produit_path(@produit)
+  end
+
  
   private
     def comment_params
